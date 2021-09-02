8e31b9759d48f496d203f73b55c2d23c013046c9
NominalTrajectory <xocpref@gmail.com>
2021-09-02 20:15:06 +0200

    Test v1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# nasa-apod-github-readme
+# Add NASA's Astronomy Picture of the day badge to your Github README
+
 This simple app allows you to add a badge with NASA's Astronomy Pircture of the Day to your Github README
---
+++ api/index.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+const fs = require('fs');
+const imageToBase64 = require('image-to-base64');
+const server = require('fastify')({ logger: false });
+
+const NASA_APOD_ENDPOINT = "https://api.nasa.gov/planetary/apod";
+const API_KEY = process.env.API_KEY;
+
+const TEMPLATE_FULL = fs.readFileSync(__dirname + '/templates/full.html', 'utf-8');
+
+
+// Helper Functions
+async function getApodData() {
+    let res = await axios.get(NASA_APOD_ENDPOINT, {
+        params: {
+            api_key: API_KEY
+        }
+    });
+
+    return res.data;
+}
+
+// Server
+server.get('/', async (request, reply) => {
+    let apodData = await getApodData();
+    let imageEncoded = await imageToBase64(apodData.url);
+
+    let currentTemplate = TEMPLATE_FULL;
+    currentTemplate = currentTemplate.replace("{{DATE}}", new Date(apodData.date).toDateString());
+    currentTemplate = currentTemplate.replace("{{IMAGE}}", `data:image/png;base64, ${imageEncoded}`);
+    currentTemplate = currentTemplate.replace("{{TITLE}}", apodData.title);
+    currentTemplate = currentTemplate.replace("{{AUTHOR}}", apodData.copyright);
+
+    reply.type('image/svg+xml');
+    reply.send(currentTemplate);
+});
+
+server.listen(5000, (err, address) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(`Server is listening at ${address}`);
+    }
+});
