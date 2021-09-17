8001ea7ede68abc0d437e959a28461805f27030c
NominalTrajectory <xocpref@gmail.com>
2021-09-17 22:51:28 +0200

    Fixed issue when APOD is a video

--- api/index.js
+++ api/index.js
@@ -15,7 +15,8 @@ let APOD_DATA_AUTO_REFRESHED = null;
 async function getApodData() {
     let res = await axios.get(NASA_APOD_ENDPOINT, {
         params: {
-            api_key: API_KEY
+            api_key: API_KEY,
+            thumbs: true,
         }
     });
 
@@ -37,16 +38,23 @@ initializeApodDataRefresher();
 // Server
 server.get('/', async (request, reply) => {
     let apodData = APOD_DATA_AUTO_REFRESHED;
+    let currentTemplate = TEMPLATE_FULL;
+    
     if(!apodData) {
         reply.send('No data available.');
     }
 
-    let imageEncoded = await imageToBase64(apodData.url);
-    let currentTemplate = TEMPLATE_FULL;
+    let mediaType = apodData['media_type'];
+    let imageEncoded = (mediaType != 'video')
+    ? await imageToBase64(apodData.url)
+    : await imageToBase64(apodData['thumbnail_url']);
+
+    if(mediaType != 'video') currentTemplate = currentTemplate.replace("Click to watch the video", "");
+
     currentTemplate = currentTemplate.replace("{{DATE}}", new Date(apodData.date).toDateString());
+    currentTemplate = currentTemplate.replace("{{TITLE}}", encodeXml(apodData.title));
+    currentTemplate = currentTemplate.replace("{{AUTHOR}}", apodData.copyright ? `by ${encodeXml(apodData.copyright)}`: "");
     currentTemplate = currentTemplate.replace("{{IMAGE}}", `data:image/png;base64, ${imageEncoded}`);
-    currentTemplate = currentTemplate.replace("{{TITLE}}", apodData.title);
-    currentTemplate = currentTemplate.replace("{{AUTHOR}}", apodData.copyright ? `by ${apodData.copyright}`: "");
 
     reply.type('image/svg+xml');
     reply.header('Cache-Control', 's-maxage=60')
@@ -60,3 +68,11 @@ server.listen(process.env.PORT || 5000, process.env.HOST || '::', (err, address)
         console.log(`Server is listening at ${address}`);
     }
 });
+
+function encodeXml(s) {
+    return (s
+        .replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&apos;')
+        .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+        .replace(/\t/g, '&#x9;').replace(/\n/g, '&#xA;').replace(/\r/g, '&#xD;')
+    );
+}
